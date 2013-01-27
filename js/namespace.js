e885f8c478a607082eed8cb18823ad647f971142
Rudem Labial <jd5688@gmail.com>
2013-01-27 12:46:47 +0800

    namespace

--- js/namespace.js
+++ js/namespace.js
@@ -1,8 +1,8 @@
 define(function() {
 	var DEM = DEM || {};
 	
-	DEM.domain 		= 	"http://localhost:8000/";
-	DEM.website		=	"http://localhost/~admin/";
+	DEM.domain 		= 	"http://localhost:8000/"; //python is listening on this port
+	DEM.website		=	"http://localhost/~admin/"; //website url here
 	
 	return DEM;
 });
